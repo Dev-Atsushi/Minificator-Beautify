ba3d836f76239b336a3ab4e2464f15477ae1d58b
The_Sushi <64918853+Dev-Atsushi@users.noreply.github.com>
2021-02-18 11:00:38 -0300

    Create interface.js

---
+++ js/interface.js
@@ -0,0 +1,31 @@
+// Não identado com gulp
+
+function hello(c) {
+    let cb = c; let res = "sushi"   ;               if(cb === res) {
+    
+    console
+    .log("Hello world")
+    
+    }else{
+    
+    console
+    .log("Not hello world")
+    }} 
+
+
+// Identado com gulp
+
+function hello(c) {
+  let cb = c;
+  let res = "sushi";
+  if (cb === res) {
+
+    console
+      .log("Hello world")
+
+  } else {
+
+    console
+      .log("Not hello world")
+  }
+}
