6593eb9cb1bdc479ef83004d901aa96519ab612f
Atsushi <atsushithesushi@gmail.com>
2021-04-29 09:55:10 -0300

    Update gulpFIle.js

--- gulpFIle.js
+++ gulpFIle.js
@@ -1,8 +1,18 @@
-const { src, dest } = require("gulp")
-const beautify = require('gulp-beautify');
+const { src, dest } = require("gulp");
+const beautify      = require('gulp-beautify');
 
-exports.js = function() {
-    return src('js/*.js')
-        .pipe(beautify.js({ indent_size: 2 }))
-        .pipe(dest('js/'))
-}
+exports.beautyjs = function() {
+  return src('js/*.js')
+    .pipe(beautify.js({ indent_size: 2 }))
+    .pipe(dest('js/'))
+};
+
+exports.minjs = function() {
+  return gulp.src('js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(javascriptObfuscator({
+      compact: true
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'))
+};
